322f8a281206bcd03d6783c1a0c259543c510546
542491514 <542491514@qq.com>
2023-02-15 16:12:04 +0800

    no message

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title("Streamlit tutorials")
+st.header("this is a header")
+st.subheader("this is a subheader")
+st.text("hello streamlit")
+st.markdown("this is markdown")
+st.success("successful")
+st.info("info")
+st.warning("warning")
+st.error("error")
+st.exception("exception 2222")
